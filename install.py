be4fffb0edf20f7b5729615f5457b796d5a22a65
Loong <69568351+Looong01@users.noreply.github.com>
2023-05-08 20:28:36 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,6 @@
 import launch
 
 
-plat = platform.system().lower()
-
-
 if not launch.is_installed("cv2"):
     print('Installing requirements for Mov2mov')
     launch.run_pip("install opencv-python", "requirements for opencv")
